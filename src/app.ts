f7ec948b9e5e70477752c05e00d539a50445dde8
Endo-Yuanteng <210224788+Endo-Yuanteng@users.noreply.github.com>
2026-02-13 14:37:54 +0900

    自販機のマーカーを追加、wsのポップアップを追加

--- src/app.ts
+++ src/app.ts
@@ -58,20 +58,20 @@ map.on("click", (e) => {
 });
 // ウォーターサーバーのボタンの機能実装
 const hiddenWSMarkers: L.Marker[] = [
-  L.marker([1560,1560]),
-  L.marker([1351,2680]),
-  L.marker([1351,2650]),
-  L.marker([1494,2823]),
-  L.marker([1529,2688]),
-  L.marker([1567,2689]),
-  L.marker([1668,2991]),
-  L.marker([1390,2065]),
-  L.marker([1645,2200]),
-  L.marker([1026,2812]),
-  L.marker([1160,1318]),
-  L.marker([1500,860]),
-  L.marker([1340,2788]),
-  L.marker([505,2826]),
+  L.marker([1560,1560]).bindPopup("駒場図書館 1F"),
+  L.marker([1351,2680]).bindPopup("コミプラ 1F"),
+  L.marker([1351,2650]).bindPopup("コミプラ 2F"),
+  L.marker([1494,2823]).bindPopup("キャンパスプラザA棟 1F"),
+  L.marker([1529,2688]).bindPopup("第2体育館 1F"),
+  L.marker([1567,2689]).bindPopup("第2体育館 2F"),
+  L.marker([1668,2991]).bindPopup("第1体育館 2F"),
+  L.marker([1390,2065]).bindPopup("8号館 1F"),
+  L.marker([1645,2200]).bindPopup("21 KOMCEE West B1F"),
+  L.marker([1026,2812]).bindPopup("5号館 1F"),
+  L.marker([1160,1318]).bindPopup("13号館 1F"),
+  L.marker([1500,860]).bindPopup("15号館 1F"),
+  L.marker([1340,2788]).bindPopup("生協購買部"),
+  L.marker([505,2826]).bindPopup("数理科学研究科棟 1F"),
 ];
 let wsMarkersVisible = false;
 const wsbtn = document.getElementById("wsMarker")!;
@@ -86,9 +86,16 @@ wsbtn.addEventListener("click", () => {
   wsMarkersVisible = !wsMarkersVisible;
 });
 // 自動販売機のボタンの機能実装
+const orangeIcon = L.icon({
+  iconUrl: "/src/assets/marker-icon-orange.png",
+  iconSize: [25,41],
+  iconAnchor: [12.5,41]
+});
 const hiddenVMMarkers: L.Marker[] = [
-  L.marker([1060,1496]),
-  L.marker([1088,1648]),
+  L.marker([1097,1504], {icon: orangeIcon}),
+  L.marker([1096,1625], {icon: orangeIcon}),
+  L.marker([1762,2443], {icon: orangeIcon}),
+  L.marker([1686,2536], {icon: orangeIcon}),
 ];
 let vmMarkersVisible = false;
 const vmbtn = document.getElementById("vmMarker")!;
